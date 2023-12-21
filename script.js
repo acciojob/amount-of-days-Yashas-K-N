1d0b74b94fb96b5eac40ee6481d027d27164b36f
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2023-12-21 22:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
 	if ((year%4 == 0 && year%100 !=0) || year%400 == 0){
-	    return 366;
+	    console.log( "366");
 	}
 		else {
-		return 365;
+		 console.log( "365");
 	}
 
 }
-console.log(return);
 daysOfAYear();
