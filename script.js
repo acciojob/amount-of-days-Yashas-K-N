0005bdc6f4e4e90c934ed39ca8b9f6c8f8b6d5ad
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2024-01-01 22:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 function daysOfAYear(year) {
-	if (year%4 == 0){
-		if(year%100 != 0){
+	if (year%4 == 0 && year%100 != 0 || year%400 == 0)
+		{
 	    console.log("366");
 	}
-		else if(year%400 == 0){
+		/*else if(year%400 == 0){
 			console.log("366");
-		}
+		}*/
 
 		else  {
 		 console.log("365");
